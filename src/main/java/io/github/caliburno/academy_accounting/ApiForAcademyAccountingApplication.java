3f2099d4c3aba8aee280e00ae29d5a5b1a7aeded
Caliburno <dario.buffa@gmail.com>
2026-01-17 16:43:22 -0300

    First commit

---
+++ src/main/java/io/github/caliburno/academy_accounting/ApiForAcademyAccountingApplication.java
@@ -0,0 +1,13 @@
+package io.github.caliburno.academy_accounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiForAcademyAccountingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiForAcademyAccountingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/caliburno/academy_accounting/ApiForAcademyAccountingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.caliburno.academy_accounting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiForAcademyAccountingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
